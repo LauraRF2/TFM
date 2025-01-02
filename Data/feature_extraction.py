2a45fb29fd83c904afbbc50104411807854b9dea
LauraRF2 <laura.rf23@gmail.com>
2025-01-02 12:38:50 +0100

    Delete Data/feature_extraction.py

--- Data/feature_extraction.py
+++
@@ -1,3 +0,0 @@
-pca_features= ['PCA1', 'PCA2', 'PCA3', 'PCA4', 'PCA5', 'PCA6', 'PCA7', 'PCA8', 'PCA9', 'PCA10', 'PCA11', 'PCA12', 'PCA13', 'PCA14', 'PCA15', 'PCA16', 'PCA17', 'PCA18', 'PCA19', 'PCA20', 'PCA21']
-mfa_features=['MFA1', 'MFA2', 'MFA3', 'MFA4', 'MFA5', 'MFA6', 'MFA7']
-tsne_features=['tSNE1', 'tSNE2', 'tSNE3']
